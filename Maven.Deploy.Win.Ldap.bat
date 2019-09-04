bc8c7d80361484eb33b4aa980720ca3607f9da1a
HY-ZhengWei <hy.zhengwei@qq.com>
2019-09-04 16:27:18 +0800

    1.0.6
    1. 升级xjava包的版本

--- Maven.Deploy.Win.Ldap.bat
+++ Maven.Deploy.Win.Ldap.bat
@@ -1,2 +1,2 @@
-start mvn deploy:deploy-file -Dfile=hy.common.ldap.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/ldap/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
-start mvn deploy:deploy-file -Dfile=hy.common.ldap-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/ldap/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:9015/nexus/content/repositories/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.ldap.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/ldap/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
+start mvn deploy:deploy-file -Dfile=hy.common.ldap-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/ldap/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
