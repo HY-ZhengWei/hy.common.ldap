4f1301a831c02842a059cd585bbbddbde9ddc37f
HY-ZhengWei <HY.ZhengWei@qq.com>
2021-03-29 14:25:17 +0800

    1.0.6
    1. 添加：Gitee.com 代码托管

--- Maven.Deploy.Mac.Ldap.sh
+++ Maven.Deploy.Mac.Ldap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn deploy:deploy-file -Dfile=hy.common.ldap.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/ldap/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
-mvn deploy:deploy-file -Dfile=hy.common.ldap-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/ldap/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/nexus/content/repositories/thirdparty
+mvn deploy:deploy-file -Dfile=hy.common.ldap.jar                              -DpomFile=./src/META-INF/maven/org/hy/common/ldap/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
+mvn deploy:deploy-file -Dfile=hy.common.ldap-sources.jar -Dclassifier=sources -DpomFile=./src/META-INF/maven/org/hy/common/ldap/pom.xml -DrepositoryId=thirdparty -Durl=http://218.21.3.19:1481/repository/thirdparty
